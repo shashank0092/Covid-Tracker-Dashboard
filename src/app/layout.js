6d3c45c3f2329fe5cbd7007b83fadd102a1a992e
SHASHANK <shashank7016204503@gmail.com>
2023-06-23 10:44:38 +0530

    Solve Error

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 'use client'
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { NextAuthProvider } from './provider'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,9 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NextAuthProvider >
+       
         {children}
-        </NextAuthProvider>
+        
         
         </body>
     </html>
